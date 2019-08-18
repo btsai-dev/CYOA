0d2eecf56883c31773a0d45a402d015283d98b98
godonan <godonanabt@gmail.com>
2019-08-17 23:19:54 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/compilingyourownapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.compilingyourownapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void go_home(View view){
+        setContentView(R.layout.activity_main);
+    }
+
+    public void enter_decision_default(View view){
+        setContentView(R.layout.decision_default_file);
+    }
+
+    public void decision_default_01(View view){
+        System.out.println("DEFAULT OUT 01");
+        setContentView(R.layout.decision_default_result_01);
+    }
+
+    public void decision_default_02(View view) {
+        System.out.println("DEFAULT OUT 02");
+        setContentView(R.layout.decision_default_result_02);
+
+    }
+}
